9fdae1a856fe0b2b5e60344a46798c73a0c0670b
Bharath Reddy <reddyjayabharath95@gmail.com>
2023-05-17 14:01:45 +0530

    topbar modifications

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
